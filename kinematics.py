d8c1addd235e3e9326b7c7af109036d93ef5f0b4
ABREDIT02 <146046749+ABREDIT02@users.noreply.github.com>
2025-06-16 22:59:11 +0530

    Add files via upload

---
+++ kinematics.py
@@ -0,0 +1,41 @@
+import numpy as np
+L1=1.0
+L2=1.0
+
+
+#gives catesian coordinates of the end effector of SCARA ARM
+def forward_kinematics(theta1,theta2):
+    x=L1*np.cos(theta1)+L2*np.cos(theta1+theta2)
+    y=L1*np.sin(theta1)+L2*np.sin(theta1+theta2)
+
+
+    return(x,y)
+
+
+
+def inverse_kinematics(x,y):
+    val=(x**2+y**2-L1**2-L2**2)/2*L1*L2
+    theta2=np.arccos(val)
+
+
+    k1=L1+L2*np.cos(theta2)
+    k2=L2*np.sin(theta2)
+
+
+
+    theta1=np.arctan2(y,x)-np.arctan2(k2,k1)
+
+    return (theta1,theta2)
+
+
+
+theta1=np.radians(30)
+theta2=np.radians(40)
+
+
+x,y=forward_kinematics(theta1,theta2)
+
+
+t1,t2=inverse_kinematics(x,y)
+print(theta1,theta2)
+print(t1,t2)
